ac16cbf4a47b09a5dd64ac2ad043e37daa04f103
Karol Blazejewski <karolblaz89@gmail.com>
2022-08-07 22:18:09 +0100

    Initial version

---
+++ kodilla-code/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+
+    public static void main(String[]args) {
+        int age = 32 ;
+        double height = 1.75 ;
+        char firstLetterSurname = 'B' ;
+        long hairs = 1000000000000L ;
+        float waterBody = 5.5F ;
+        boolean woman = false ;
+        System.out.println("Karol");
+        System.out.println(age) ;
+        System.out.println(height);
+        System.out.println(firstLetterSurname);
+        System.out.println(hairs);
+        System.out.println(waterBody);
+        System.out.println(woman);
+    }
+}
---
+++ kodilla-code/prawdaFalsz.java
@@ -0,0 +1,11 @@
+public class prawdaFalsz {
+    public static void main(String[] args) {
+        System.out.println("Jesli jeden jest wieksze niz dwa to sie wyswietli komunikat Hurra");
+        if (1>2) {
+            System.out.println("Hurra jeden jest wieksze niz dwa");
+        } else {
+            System.out.println("wszystko po staremu jeden jest mniejsze niz dwa");
+        }
+
+    }
+}
