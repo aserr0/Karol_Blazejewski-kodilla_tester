18fbf901dc7d09b67097d563c5d0c6a845c33814
Karol Blazejewski <karolblaz89@gmail.com>
2022-08-07 22:39:25 +0100

    Initial version

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int wiek = 32 ;
+        char PierwszaLiteraNazwiska = 'B' ;
+        System.out.println("HelloWorld!");
+        System.out.println("Karol");
+        System.out.println(wiek);
+        System.out.println(PierwszaLiteraNazwiska);
+    }
+
+}
