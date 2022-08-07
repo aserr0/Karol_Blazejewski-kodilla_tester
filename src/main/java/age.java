db972a656f12e72da73381eba831ceca79af056e
Karol <karolblaz89@gmail.com>
2022-08-07 17:11:50 +0100

    Initial version

---
+++ src/main/java/age.java
@@ -0,0 +1,17 @@
+public class age {
+    public static void main(String[] args) {
+        int yourAge = 12;
+        System.out.println(yourAge);
+
+
+        if (yourAge < 10) {
+            System.out.println("You're a kid");
+        } else if (yourAge == 12) {
+            System.out.println("You're twelve years old ");
+        } else {
+            System.out.println("You're an adult");
+        }
+
+
+    }
+}
