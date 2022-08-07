de0452f72f991102db80c9ef87e10fd5872ff3de
Karol <karolblaz89@gmail.com>
2022-08-07 15:04:05 +0100

    Initial version

---
+++ src/main/java/DebugExample.java
@@ -0,0 +1,22 @@
+public class DebugExample {
+            public static void main(String[] args) {
+            int firstNumber = 11;
+            int secondNumber = 22;
+
+            System.out.println(firstNumber);
+            System.out.println(secondNumber);
+
+            sumAndDisplay(firstNumber, secondNumber);
+        }
+
+        private static void sumAndDisplay(int firstNumber, int secondNumber) {
+            int result = firstNumber + secondNumber;
+            System.out.println(result);
+        }
+
+
+    }
+
+
+
+
