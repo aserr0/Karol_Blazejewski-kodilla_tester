f1638e34abeaa533b8f56396bb820d363b55fdd3
Karol Blazejewski <karolblaz89@gmail.com>
2022-08-14 13:19:43 +0100

    Initial version

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,17 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook(600, 1000);
+        System.out.println("notebook.weight: " + notebook.weight);
+        System.out.println("notebook.price: " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook(2000, 1500);
+        System.out.println("heavyNotebook.weight: " + heavyNotebook.weight + " " + "heavyNotebook.price: " + heavyNotebook.price );
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook(1600, 500);
+        System.out.println("oldNotebook.weight: " + oldNotebook.weight);
+        System.out.println("oldNotebook.price: " + oldNotebook.price);
+        oldNotebook.checkPrice();
     }
 }
