b8d4885be5b6b1f3e9f5dc669b45a56472967fa1
Karol Blazejewski <karolblaz89@gmail.com>
2022-08-16 19:15:59 +0100

    Initial version

--- src/main/java/Grades.java
+++ src/main/java/Grades.java
@@ -1,18 +1,37 @@
 public class Grades {
-    public int[] grades;
-    int size;
+    public static void main(String[] args) {
 
-    public Grades() {
-        this.grades = new int[10];
-        this.size = 0;
-    }
-    public void add(int value) {
-        if (this.size == 10) {
-            return;
+        int[] grades = {5, 4, 3, 2, 1, 5, 4, 3, 2, 1};
+
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
         }
-        this.grades[this.size] = value;
-        this.size++;
-    }
 
+        sum/= grades.length;
+        System.out.println(grades[sum]);
+        }
 
+        public int[] grades;
+        int size;
+
+        public Grades() {
+            this.grades = new int[10];
+            this.size = 0;
+        }
+
+        public void add(int value) {
+            if (this.size == 10) {
+                return;
+            }
+            this.grades[this.size] = value;
+            this.size++;
+        }
 }
+
+
+
+
+
+
+
