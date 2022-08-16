d20614e5ca99aa38174f07aeced5b1490ae42171
Karol Blazejewski <karolblaz89@gmail.com>
2022-08-16 15:42:52 +0100

    Initial version

---
+++ src/main/java/Grades.java
@@ -0,0 +1,18 @@
+public class Grades {
+    public int[] grades;
+    int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+
+}
